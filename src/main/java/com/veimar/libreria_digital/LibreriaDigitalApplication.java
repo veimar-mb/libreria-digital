222ac014eda1b8a8b34c78eeddcf95f6fce5de00
Martínez <veimar.martinez@accenture.com>
2024-11-21 10:34:34 -0600

    init commit

---
+++ src/main/java/com/veimar/libreria_digital/LibreriaDigitalApplication.java
@@ -0,0 +1,13 @@
+package com.veimar.libreria_digital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibreriaDigitalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibreriaDigitalApplication.class, args);
+	}
+
+}
